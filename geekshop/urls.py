74c3de4ab93f23b3b6231d24524512b7bc7005d2
lockorsa <lockorsa@gmail.com>
2021-10-21 22:53:03 +0700

    Tasks completed

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from geekshop import views
 
 urlpatterns = [
-    path('', views.index),
-    path('products/', views.products),
-    path('contact/', views.contact),
+    path('', views.index, name='index'),
+    path('products/', views.products, name='products'),
+    path('contacts/', views.contact, name='contacts'),
 ]
