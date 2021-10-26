0d95ee8d0bcd67341fd4c3f185cd6daa1b03b024
lockorsa <lockorsa@gmail.com>
2021-10-27 05:53:01 +0700

    Контроллер и шаблон категорий

--- geekshop/views.py
+++ geekshop/views.py
@@ -23,6 +23,7 @@ def products(request):
 
 def product_category(request, slug: str):
     context = {
-        'category': ProductCategory.objects.get(slug=slug),
+        'categories': ProductCategory.objects.all(),
+        'current_category': ProductCategory.objects.get(slug=slug),
     }
-    return render(request, 'geekshop/products.html', context=context)
+    return render(request, 'geekshop/product_category.html', context=context)
